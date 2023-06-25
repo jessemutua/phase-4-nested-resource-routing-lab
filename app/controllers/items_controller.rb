9169b4938af4401c26e72ef5c4a857091ef154fa
Jesse <jessemutua76@gmail.com>
2023-06-25 16:31:16 +0300

    Completed assignment

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,34 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
+    if params[:user_id]
+      user=User.find(params[:user_id])
+      items=user.items
+    else
+       items = Item.all
+    end
     render json: items, include: :user
   end
+ def show
+  item = Item.find_by(id: params[:id])
+  render json: item, include: :user
+ end
+
+ def create
+    user = User.find(params[:user_id])
+    item=user.items.create!(item_params)
+    if item
+      render json: item, status: :created
+
+    else
+      render json: {error: item.errors.full_messages }, status: :unprocessable_entity
+    end
+ end
+ private
+ def item_params
+  params.require(:item).permit(:name, :description, :price, :user_id)
+ end
 
 end
+
+
